292344219670c9591dce680f87954fb791d010cc
William Aguera <williamaguera.m@hotmail.com>
2022-04-06 11:25:56 -0300

    fix port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const perPage = 5;
 
 app.get("/posts", (_req, res) => {
